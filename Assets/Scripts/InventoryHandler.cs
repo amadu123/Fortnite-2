bf7d2c3b0a5a63a26bb2515db88f473110965ba3
Eddie Zhuang <zhuang.eddie@gmail.com>
2021-08-12 22:00:57 -0400

    Add blueprint model

--- Assets/Scripts/InventoryHandler.cs
+++ Assets/Scripts/InventoryHandler.cs
@@ -8,12 +8,16 @@ public class InventoryHandler : MonoBehaviour
     BuildingHandler buildingHandler;
     CombatHandler combatHandler;
     PhotonView PV;
+    public GameObject gun;
+    public GameObject blueprint;
 
     void Start()
     {
         PV = GetComponent<PhotonView>();
         buildingHandler = GetComponent<BuildingHandler>();
         combatHandler = GetComponent<CombatHandler>();
+        gun.SetActive(true);
+        blueprint.SetActive(false);
     }
 
     void Update()
@@ -24,21 +28,29 @@ public class InventoryHandler : MonoBehaviour
         {
             buildingHandler.ChangeBuildMode("Floor");
             combatHandler.ChangeCombatMode("None");
+            gun.SetActive(false);
+            blueprint.SetActive(true);
         }
         else if (Input.GetKeyDown(KeyCode.F2))
         {
             buildingHandler.ChangeBuildMode("Wall");
             combatHandler.ChangeCombatMode("None");
+            gun.SetActive(false);
+            blueprint.SetActive(true);
         }
         else if (Input.GetKeyDown(KeyCode.F3))
         {
             buildingHandler.ChangeBuildMode("Stair");
             combatHandler.ChangeCombatMode("None");
+            gun.SetActive(false);
+            blueprint.SetActive(true);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             buildingHandler.ChangeBuildMode("None");
             combatHandler.ChangeCombatMode("Assault Rifle");
+            gun.SetActive(true);
+            blueprint.SetActive(false);
         }
     }
 }
